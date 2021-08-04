2e271b9c1cf5aff7e8f83fe70d4d84c8faf2bdd4
sunn-e <sunnydhoke22@gmail.com>
2021-08-04 22:34:43 +0530

    add cell to homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,14 @@
 // import { Link, routes } from '@redwoodjs/router'
-import BlogLayout from 'src/layouts/BlogLayout'
+import BlogLayout from "src/layouts/BlogLayout";
+import BlogPostsCell from "src/components/BlogPostsCell/BlogPostsCell.js";
+
 const HomePage = () => {
   return (
     <BlogLayout>
       <p>Taking Fullstack to the Jamstack</p>
+      <BlogPostsCell />
     </BlogLayout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
